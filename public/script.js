2999a2e491b99ada07f3a4794fb9372dabcda129
Miro-dev <miroslav.ivanov.iliev@gmail.com>
2020-02-08 11:01:58 +0100

    Fixing netlify deployment (Moved html file to main folder).

--- public/script.js
+++ public/script.js
@@ -1,13 +1,3 @@
-// const url = "/battle";
-
-// $(".btn").click(function(e) {
-//   $.get(url, function(data, status) {
-//     console.log(data);
-//     document.getElementById("postBoard").append(data);
-//   });
-//   e.preventDefault();
-// });
-
 $(".btn").click(function(e) {
   const textarea = document.getElementById("postBoard");
   $.getJSON("/battle", function(data) {
